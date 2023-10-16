027316e2e8b4042af22cebf689e7b9d856b948cb
aldith19 <aldith.reynaldi23@gmail.com>
2023-10-16 15:17:45 +0700

    Java Standard Classes

---
+++ src/Java/Classes/StringApp.java
@@ -0,0 +1,16 @@
+package Java.Classes;
+
+public class StringApp {
+    public static void main(String[] args) {
+        String name = "Malik";
+        String name1 = "Bagus";
+        String lowercase = name.toLowerCase();
+        String uppercase = name1.toUpperCase();
+
+        System.out.println(name);
+        System.out.println(lowercase);
+        System.out.println(name1);
+        System.out.println(uppercase);
+    }
+
+}
